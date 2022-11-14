7d407ca58ff59f1a40fa5e2db0c114e504b02d23
George Kolev <gogo1520180@gmail.com>
2022-11-14 12:23:14 +0200

    font config

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,6 +1,6 @@
 <template lang="">
     <div class="w-screen h-screen flex justify-center items-center">
-        <h1 class="text-primary">VNN</h1>
+        <h1 class="text-primary text-3xl">VNN</h1>
     </div>
 </template>
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,48 @@ export default {
       colors: {
         primary: '#42a7f5'
       }
+    },
+    fontSize: {
+      'xl': ['1.5rem', {
+        lineHeight: '2rem',
+        letterSpacing: '-0.01em',
+        fontWeight: '500',
+      }],
+      '2xl': ['2rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+      '3xl': ['3rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+      '4xl': ['4rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+      '5xl': ['5rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+      '6xl': ['6rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+      '7xl': ['7rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
+      '8xl': ['8rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }],
     }
   }
 }
